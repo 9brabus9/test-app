b4d59204f9a6e69563955edb4ef0790fe2dc396a
user <user@users-iMac.local>
2018-07-31 12:06:40 +0300

    signed up done

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Button, View, StyleSheet} from 'react-native';
+import { Button, View, Text, StyleSheet} from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import t from 'tcomb-form-native'; // 0.6.9
+import Hihomie from './Hihomie';
 
 
 const Form = t.form.Form;
@@ -10,39 +11,44 @@ const User = t.struct({
   email: t.String,
   username: t.String,
   password: t.String,
-  terms: t.Boolean
+  //terms: t.Boolean
 });
 
- class App extends Component {
-  handleSubmit = () => {
-    const value = this._form.getValue(); // use that ref to get the form value
-   
-    if(value!==null&&value.terms===true){
-      alert("fgsdgfd");
-    }
-    else alert("screw YOU");
+
+class App extends Component {
+handleSubmit = () => {
+  const value = this._form.getValue(); // use that ref to get the form value
+  
+  if(value!==null){
+    this.props.navigation.navigate('Details');
   }
-  render() {
-    return (
-      <View style={styles.container}>
-         <Form 
-          ref={c => this._form = c} // assign a ref
-          type={User} 
-        />
-        <Button
-          title="Sign Up!"
-          onPress={this.handleSubmit}
-        />
-      </View>
-    );
-  }
+  else alert("screw YOU");
+}
+render() {
+  return (
+    <View style={styles.container}>
+        <Form 
+        ref={c => this._form = c} // assign a ref
+        type={User} 
+      />
+      <Button
+        title="Sign Up!"
+        onPress={this.handleSubmit}
+      />
+    </View>
+  );
+}
 }
 
-const RootStack = createStackNavigator({
-  Home: {
-    screen: App
+const RootStack = createStackNavigator(
+  {
+    Home: App,
+    Details: Hihomie,
   },
-});
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 export default class App extends React.Component {
   render() {
